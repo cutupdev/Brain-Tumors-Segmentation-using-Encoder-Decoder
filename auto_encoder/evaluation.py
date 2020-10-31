2735c5f1428a330af3d8a5314249c081638ac742
CatLover <takasi75926@gmail.com>
2020-10-30 21:56:56 -0700

    defined smooth for dice error

--- auto_encoder/evaluation.py
+++ auto_encoder/evaluation.py
@@ -15,6 +15,7 @@ import keras.backend as K
 from sklearn.utils.extmath import cartesian
 
 def dice_coef(y_true, y_pred):
+    smooth = 1
     y_true_f = K.flatten(y_true)
     y_pred_f = K.flatten(y_pred)
     intersection = K.sum(y_true_f * y_pred_f)
